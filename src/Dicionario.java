df8b4f286894a3c585f6d2068b5c6b6ec3760d0f
Leticia Lopes Lima <leticiazlzlima@gmail.com>
2025-01-23 00:54:59 -0300

    :sparkles: feat: implementa método traduzirParaPortugues

--- src/Dicionario.java
+++ src/Dicionario.java
@@ -1,16 +1,45 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Dicionario {
-    public String idioma; 
+        public String idioma; 
+        public String termo;
+        public Scanner in;
 
     //definição de parâmetro
     //falta colocar o try and catch
     public Dicionario (String idioma){
+        // try {
         this.idioma = idioma;
+        try {
+            in = new Scanner(new File("csv/" + idioma + ".csv"));
+            System.out.println("Dicionário do idioma " + idioma + " está disponível");
+
+        } 
+        catch (FileNotFoundException ex) {
+            System.out.println("Arquivo não encontrado!" );
+
+            }
     }   
 
     public void traduzirParaPortugues (String termo) {
-		
+        this.termo = termo;
+        String valor = "";
+
+        //falta colocar exceção aqui
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            String[] linha = line.split(";");
+            if (linha[0].equals(termo)) {
+                valor = linha[1];
+        
+            } else {
+                valor = "Tradução inexistente";
+            }
+        }
+        System.out.println(valor);
+
     }
 
     public void traduzirParaIdioma (String termo) {
@@ -32,7 +61,6 @@ public class Dicionario {
     public void setIdioma (String idioma) {
 		    //falta colocar o try and catch
 
-    }
+    };
 
-
-}
+};
