4b25703e6726e58b4db4ebe32615f29c5a6d5ed1
Leticia Lopes Lima <leticiazlzlima@gmail.com>
2025-01-23 11:02:23 -0300

    :sparkles: feat: cria arquivo de teste

---
+++ TesteTraduzir.java
@@ -0,0 +1,17 @@
+
+public class TesteTraduzir {
+    public static void main(String[] args) {
+            // Configura o Scanner para ler o arquivo ingles.csv
+            // Scanner scanner = new Scanner(new File("src/csv/ingles.csv"));
+
+            // Instancia a classe Dicionario
+            Dicionario dicionario = new Dicionario("ingles");
+
+            // Testa o método com palavras do arquivo
+            //dicionario.traduzirParaPortugues("computer"); // Esperado: computador
+            //dicionario.traduzirParaPortugues("keyboard"); // Esperado: teclado
+            // dicionario.traduzirParaPortugues("nonexistent"); // Esperado: erro
+
+            dicionario.traduzirParaIdioma("computor");
+
+        }}
