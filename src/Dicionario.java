dcc916788b4963d019ec36d21272d474690dade7
Leticia Lopes Lima <leticiazlzlima@gmail.com>
2025-01-22 22:48:04 -0300

    :tada: Commit inicial

---
+++ src/Dicionario.java
@@ -0,0 +1,38 @@
+
+
+public class Dicionario {
+    public String idioma; 
+
+    //definição de parâmetro
+    //falta colocar o try and catch
+    public Dicionario (String idioma){
+        this.idioma = idioma;
+    }   
+
+    public void traduzirParaPortugues (String termo) {
+		
+    }
+
+    public void traduzirParaIdioma (String termo) {
+		
+    }
+
+    public void localizarPalavraIdioma (String termo) {
+		
+    }
+
+    public void localizarPalavraPortugues (String termo) {
+		
+    }
+
+    public void getIdiomas () {
+		
+    }
+
+    public void setIdioma (String idioma) {
+		    //falta colocar o try and catch
+
+    }
+
+
+}
